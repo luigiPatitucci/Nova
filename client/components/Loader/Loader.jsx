e69423d7400af2073a753b816b5f2659ea5179b2
Kevin <kevinvega2070@gmail.com>
2020-11-15 17:24:42 -0300

    Added test animation when preloading app resources.

--- client/components/Loader/Loader.jsx
+++ client/components/Loader/Loader.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import LottieView from 'lottie-react-native';
 
 
 const Loader = () => {
     return (
-        <View style={{justifyContent: 'center', alignItems: 'center'}}>
-            <Text>
-                Estoy cargando
-            </Text>
-        </View>
+        <LottieView source={require('../../assets/5215-loading-checkmark.json')} autoPlay loop />
     )
 };
 
